22ff63bc2bbf60a7a527d65d8aefcedbf465ded0
Sachin Ravi <sachinr@twitter.com>
2016-10-12 18:44:58 -0400

    converted tabs to spaces

--- graph/plot_loss.py
+++ graph/plot_loss.py
@@ -5,36 +5,36 @@ FILE1 = 'stdout_simpleConv_2layerLstm_noBN'
 FILE2 = 'stdout_BN'
 
 def readFile(fileName):
-	iters = []
-	losses = []
+   iters = []
+   losses = []
 
-	with open(fileName) as f:
-		for line in f:
-			arr = line.split()
-			if len(arr) >= 5 and arr[0] == 'Dataset:' and arr[3] == 'Loss:':
-				iters.append(int(arr[1][:-1]))
-				
-				loss = arr[4]
-				if loss.find(",") > 0:
-					loss = loss.replace(',', '')
-				losses.append(float(loss))
-					
+   with open(fileName) as f:
+      for line in f:
+         arr = line.split()
+         if len(arr) >= 5 and arr[0] == 'Dataset:' and arr[3] == 'Loss:':
+            iters.append(int(arr[1][:-1]))
+            
+            loss = arr[4]
+            if loss.find(",") > 0:
+               loss = loss.replace(',', '')
+            losses.append(float(loss))
+               
 
-	return iters, losses
+   return iters, losses
 
 if __name__ == '__main__':
-	args = []
-	for i in range(1, len(sys.argv)):
-		iters, losses = readFile(sys.argv[i])
-		plt.plot(iters, losses, label=sys.argv[i])
-		#args.append(iters)
-		#args.append(losses)
-		#args.append(readFile(sys.argv[i]))
+   args = []
+   for i in range(1, len(sys.argv)):
+      iters, losses = readFile(sys.argv[i])
+      plt.plot(iters, losses, label=sys.argv[i])
+      #args.append(iters)
+      #args.append(losses)
+      #args.append(readFile(sys.argv[i]))
 
-	
-	#iters1, losses1 = readFile(FILE1)
-	#iters2, losses2 = readFile(FILE2)
-	#plt.plot(iters1, losses1, iters2, losses2)
-	plt.legend(loc='upper right')
-	#plt.plot(*args)
-	plt.show()
+   
+   #iters1, losses1 = readFile(FILE1)
+   #iters2, losses2 = readFile(FILE2)
+   #plt.plot(iters1, losses1, iters2, losses2)
+   plt.legend(loc='upper right')
+   #plt.plot(*args)
+   plt.show()
--- graph/plot_validation.py
+++ graph/plot_validation.py
@@ -5,34 +5,34 @@ FILE1 = 'stdout_simpleConv_2layerLstm_noBN'
 FILE2 = 'stdout_BN'
 
 def readFile(fileName):
-	iters = []
-	vals = []
+   iters = []
+   vals = []
 
-	with open(fileName) as f:
-		iterFound = False
-		iteration = 0
-		for line in f:
-			arr = line.split()
-			if len(arr) >= 5 and arr[0] == 'Dataset:' and arr[3] == 'Loss:':
-				iteration = int(arr[1][:-1])
-				iterFound = True		
+   with open(fileName) as f:
+      iterFound = False
+      iteration = 0
+      for line in f:
+         arr = line.split()
+         if len(arr) >= 5 and arr[0] == 'Dataset:' and arr[3] == 'Loss:':
+            iteration = int(arr[1][:-1])
+            iterFound = True     
 
-			if iterFound and len(arr) >=4 and arr[1] == 'global' and arr[2] == 'accuracy:':
-				val = arr[3]
-				if val.find("%") > 0:
-					val = val.replace('%', '')
-				
-				vals.append(float(val))	
-				iters.append(iteration)
-				iterFound = False	
+         if iterFound and len(arr) >=4 and arr[1] == 'global' and arr[2] == 'accuracy:':
+            val = arr[3]
+            if val.find("%") > 0:
+               val = val.replace('%', '')
+            
+            vals.append(float(val)) 
+            iters.append(iteration)
+            iterFound = False 
 
-	return iters, vals 
+   return iters, vals 
 
 if __name__ == '__main__':
-	args = []
-	for i in range(1, len(sys.argv)):
-		iters, vals = readFile(sys.argv[i])
-		plt.plot(iters, vals, label=sys.argv[i])
-			
-	plt.legend(loc='lower right')
-	plt.show()
+   args = []
+   for i in range(1, len(sys.argv)):
+      iters, vals = readFile(sys.argv[i])
+      plt.plot(iters, vals, label=sys.argv[i])
+         
+   plt.legend(loc='lower right')
+   plt.show()
