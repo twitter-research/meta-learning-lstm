fea309b75a9103ebdae701b772f9a22436a75bc7
Sachin Ravi <sachinr@twitter.com>
2016-10-12 18:44:58 -0400

    converted tabs to spaces

--- graph/plot_all.py
+++ graph/plot_all.py
@@ -5,60 +5,60 @@ FILE1 = 'stdout_simpleConv_2layerLstm_noBN'
 FILE2 = 'stdout_BN'
 
 def readFile(fileName):
-	iters = []
-	train_vals = []
-	oneShot_vals = []
-	fiveShot_vals = []
+   iters = []
+   train_vals = []
+   oneShot_vals = []
+   fiveShot_vals = []
 
-	with open(fileName) as f:
-		iterFound = False
-		trainFound = False
-		oneFound = False
+   with open(fileName) as f:
+      iterFound = False
+      trainFound = False
+      oneFound = False
 
-		iteration = 0
-		for line in f:
-			arr = line.split()
-			if len(arr) >= 5 and arr[0] == 'Dataset:' and arr[3] == 'Loss:':
-				iteration = int(arr[1][:-1])
-				iterFound = True		
+      iteration = 0
+      for line in f:
+         arr = line.split()
+         if len(arr) >= 5 and arr[0] == 'Dataset:' and arr[3] == 'Loss:':
+            iteration = int(arr[1][:-1])
+            iterFound = True     
 
-			if iterFound and trainFound and oneFound and len(arr) >=4 and arr[1] == 'global' and arr[2] == 'accuracy:':
-				val = arr[3]
-				if val.find("%") > 0:
-					val = val.replace('%', '')
-				
-				fiveShot_vals.append(float(val))	
- 				iters.append(iteration)
-				
-				iterFound = False
-				trainFound = False
-				oneFound = False
+         if iterFound and trainFound and oneFound and len(arr) >=4 and arr[1] == 'global' and arr[2] == 'accuracy:':
+            val = arr[3]
+            if val.find("%") > 0:
+               val = val.replace('%', '')
+            
+            fiveShot_vals.append(float(val)) 
+            iters.append(iteration)
+            
+            iterFound = False
+            trainFound = False
+            oneFound = False
 
-			elif iterFound and trainFound and len(arr) >=4 and arr[1] == 'global' and arr[2] == 'accuracy:':
-				val = arr[3]
-				if val.find("%") > 0:
-					val = val.replace('%', '')
-				
-				oneShot_vals.append(float(val))	
-				oneFound = True
+         elif iterFound and trainFound and len(arr) >=4 and arr[1] == 'global' and arr[2] == 'accuracy:':
+            val = arr[3]
+            if val.find("%") > 0:
+               val = val.replace('%', '')
+            
+            oneShot_vals.append(float(val))  
+            oneFound = True
 
-			elif iterFound and len(arr) >=4 and arr[1] == 'global' and arr[2] == 'accuracy:':
-				val = arr[3]
-				if val.find("%") > 0:
-					val = val.replace('%', '')
-				
-				train_vals.append(float(val))	
-				trainFound = True	
-			
-	return iters, train_vals, oneShot_vals, fiveShot_vals 
+         elif iterFound and len(arr) >=4 and arr[1] == 'global' and arr[2] == 'accuracy:':
+            val = arr[3]
+            if val.find("%") > 0:
+               val = val.replace('%', '')
+            
+            train_vals.append(float(val)) 
+            trainFound = True 
+         
+   return iters, train_vals, oneShot_vals, fiveShot_vals 
 
 if __name__ == '__main__':
-	args = []
-	for i in range(1, len(sys.argv)):
-		iters, train_vals, oneShot_vals, fiveShot_vals = readFile(sys.argv[i])
-		plt.plot(iters, train_vals, label=sys.argv[i] + ":train")
-		plt.plot(iters, oneShot_vals, label=sys.argv[i] + ":one-shot")
-		plt.plot(iters, fiveShot_vals, label=sys.argv[i] + ":five-shot")
-			
-	plt.legend(loc='lower right')
-	plt.show()
+   args = []
+   for i in range(1, len(sys.argv)):
+      iters, train_vals, oneShot_vals, fiveShot_vals = readFile(sys.argv[i])
+      plt.plot(iters, train_vals, label=sys.argv[i] + ":train")
+      plt.plot(iters, oneShot_vals, label=sys.argv[i] + ":one-shot")
+      plt.plot(iters, fiveShot_vals, label=sys.argv[i] + ":five-shot")
+         
+   plt.legend(loc='lower right')
+   plt.show()
--- graph/plot_sgd.py
+++ graph/plot_sgd.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
-	train = [58.3, 66.6, 95.8, 99.9, 95.8, 99.9, 99.9, 99.9, 99.9, 99.9, 99.9,
-		99.9, 99.9, 99.9, 99.9, 99.9, 99.9, 99.9, 99.9]
-	validation = [40.0, 50.6, 53.3, 58.6, 56.0, 57.3, 72, 72, 70.6, 76, 73.3, 
-		74.6, 73.3, 77, 74.6, 73.3, 74.6, 73.3, 73.3]
+   train = [58.3, 66.6, 95.8, 99.9, 95.8, 99.9, 99.9, 99.9, 99.9, 99.9, 99.9,
+      99.9, 99.9, 99.9, 99.9, 99.9, 99.9, 99.9, 99.9]
+   validation = [40.0, 50.6, 53.3, 58.6, 56.0, 57.3, 72, 72, 70.6, 76, 73.3, 
+      74.6, 73.3, 77, 74.6, 73.3, 74.6, 73.3, 73.3]
 
-	epochs = list(xrange(19))
-	plt.plot(epochs, train, label='train accuracy')
-	plt.plot(epochs, validation, label='test accuracy')
-	plt.legend(loc='upper right')
-	plt.show()	
+   epochs = list(xrange(19))
+   plt.plot(epochs, train, label='train accuracy')
+   plt.plot(epochs, validation, label='test accuracy')
+   plt.legend(loc='upper right')
+   plt.show()  
