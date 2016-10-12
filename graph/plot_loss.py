f38bd62173e13a76a56c45b3e0c2312efbc6dbd3
Sachin Ravi <sachinr@twitter.com>
2016-10-12 18:43:51 -0400

    code for plots

---
+++ graph/plot_loss.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import sys
+
+FILE1 = 'stdout_simpleConv_2layerLstm_noBN'
+FILE2 = 'stdout_BN'
+
+def readFile(fileName):
+	iters = []
+	losses = []
+
+	with open(fileName) as f:
+		for line in f:
+			arr = line.split()
+			if len(arr) >= 5 and arr[0] == 'Dataset:' and arr[3] == 'Loss:':
+				iters.append(int(arr[1][:-1]))
+				
+				loss = arr[4]
+				if loss.find(",") > 0:
+					loss = loss.replace(',', '')
+				losses.append(float(loss))
+					
+
+	return iters, losses
+
+if __name__ == '__main__':
+	args = []
+	for i in range(1, len(sys.argv)):
+		iters, losses = readFile(sys.argv[i])
+		plt.plot(iters, losses, label=sys.argv[i])
+		#args.append(iters)
+		#args.append(losses)
+		#args.append(readFile(sys.argv[i]))
+
+	
+	#iters1, losses1 = readFile(FILE1)
+	#iters2, losses2 = readFile(FILE2)
+	#plt.plot(iters1, losses1, iters2, losses2)
+	plt.legend(loc='upper right')
+	#plt.plot(*args)
+	plt.show()
---
+++ graph/plot_params.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+import sys
+
+params = ['W', 'bF', 'cI', 'WF', 'bI', 'WI']
+
+def readFileParam(fileName, param):
+	iters = []
+	vals = []
+	dataset = 'Dataset:'
+	parameter = 'Parameter'
+	
+	d = 0
+	is_param = False
+
+	with open(fileName) as f:
+		for line in f:
+			arr = line.split()
+		
+			if dataset in arr:
+				d = arr[arr.index(dataset) + 1]
+				d = d.replace(',', '')
+	
+			if parameter in arr:
+				is_param = True
+
+			if d > 0 and is_param and param in arr:
+				iters.append(d)
+				v = arr[arr.index(param) + 2]
+				v = v.replace(',', '')
+
+				vals.append(v)
+				d = 0		
+				is_param = False
+
+	return iters, vals
+
+def readFileRatio(fileName, param):
+	iters = []
+	vals = []
+	dataset = 'Dataset:'
+	parameter = 'Update/Parameter'
+	
+	d = 0
+	is_param = False
+
+	with open(fileName) as f:
+		for line in f:
+			arr = line.split()
+		
+			if dataset in arr:
+				d = arr[arr.index(dataset) + 1]
+				d = d.replace(',', '')
+	
+			if parameter in arr:
+				is_param = True
+
+			if d > 0 and is_param and param in arr:
+				iters.append(d)
+				v = arr[arr.index(param) + 2]
+				v = v.replace(',', '')
+
+				vals.append(v)
+				d = 0		
+				is_param = False
+
+	return iters, vals
+
+
+
+if __name__ == '__main__':
+	args = []
+	for param in params:
+		iters, vals = readFileParam(sys.argv[1], param)
+		plt.plot(iters, vals, label=param)
+
+	fontP = FontProperties()
+	fontP.set_size('small')
+	plt.legend(prop = fontP, loc=9, bbox_to_anchor=(0.5, 0), ncol=3)
+	#plt.plot(*args)
+	plt.show()
