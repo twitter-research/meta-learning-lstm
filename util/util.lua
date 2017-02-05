59260d1b542a5ad75df5dafe4050e39cea563fdd
Sachin Ravi <sachinravi14@gmail.com>
2017-02-05 15:52:55 -0500

    removed unused functions from util

--- util/util.lua
+++ util/util.lua
@@ -23,34 +23,6 @@ function util.localize(x, opt)
    return x
 end
 
-function util.paramNorm(params)
-   local norms = {}
-   if #params > 0 and type(params[1]) == 'table' then 
-      for i, weights in pairs(params) do
-         norms[i] = {}
-         for j, weight in pairs(weights) do  
-            norms[i][j] = math.sqrt(torch.sum(torch.pow(weight,2))) 
-         end
-      end
-   elseif #params > 0 and type(params[1]) == 'userdata' then 
-      for j, weight in pairs(params) do   
-         norms[j] = math.sqrt(torch.sum(torch.pow(weight,2))) 
-      end
-   end
-   return norms
-end
-
-function util.gradParamRatio(lr, grads, params)
-   local ratios = {}
-   for i, weights in pairs(params) do
-      ratios[i] = {}
-      for j, weight in pairs(weights) do     
-         ratios[i][j] = torch.sum(torch.pow(-grads[i][j]*lr, 2))/torch.sum(torch.pow(weight, 2)) 
-      end
-   end 
-   return ratios
-end
-
 function util.boolize(x)
     if x == 'false' then
         x = false
@@ -58,29 +30,6 @@ function util.boolize(x)
         x = true
     end
     return x
-end
-
-function util.NaNCheck(params)
-   local NaNParams = {}
-   if #params > 0 and type(params[1]) == 'table' then 
-      for i, weights in pairs(params) do
-         for j, weight in pairs(weights) do
-            local nan_mask = weight:ne(weight)
-            if torch.sum(nan_mask) > 0 then
-               table.insert(NaNParams, j)
-            end
-         end
-      end
-   elseif #params > 0 and type(params[1]) == 'userdata' then 
-      for j, weight in pairs(params) do
-         local nan_mask = weight:ne(weight)
-         if torch.sum(nan_mask) > 0 then
-            table.insert(NaNParams, j)
-         end
-      end
-   end
-   
-   return NaNParams
 end
 
 function util.deepClone(tbl)
@@ -102,12 +51,6 @@ function util.deepClone(tbl)
       return copy
    else
       return tbl
-   end
-end
-
-function util.debugPrint(s, opt)
-   if opt.debug then
-      print(s)
    end
 end
 
@@ -146,53 +89,7 @@ function util.unflattenParamsArb(exampleParams, flatParams)
    return unflattenParams
 end
 
---[[
-   If networkParams and loadParams are not the same size because loadParams 
-   is smaller, extend loadParams to matchg networkParams size by intializing
-   extra numbers randomly.
---]]
-function util.checkAndExpandParams(networkParams, loadParams)
-   if type(networkParams) == 'userdata' and type(loadParams) == 'userdata' then
-      
-      if networkParams:nElement() ~= loadParams:nElement() then
-         local oldSize = loadParams:size(1)
-         local newSize = networkParams:size(1)
-         
-         loadParams:resizeAs(networkParams:typeAs(loadParams))
-         for i=oldSize+1,newSize do
-            loadParams[i][1] = torch.uniform(-0.1, 0.1)
-         end
-      end
-   else
-      error("inputs are not both tables or both tensors") 
-   end
-   
-   --[[if type(networkParams) == 'table' and type(loadParams) == 'table' then
-      print('here')
-      assert(#networkParams == #loadParams, "Param table sizes should be equal")
-      for i=1,#networkParams do 
-         util.checkAndExpandParams(networkParams[i], loadParams[i])
-      end
-   elseif type(networkParams) == 'userdata' and type(loadParams) == 'userdata' then
-      if networkParams:nElement() ~= loadParams:nElement() then
-         loadParams:resizeAs(networkParams:typeAs(loadParams))
-      end
-   else
-      error("inputs are not both tables or both tensors")
-   end--]]
-
-   return loadParams
-end
-
-function util.MSRinit(model)
-   for k,v in pairs(model:findModules('nn.SpatialConvolution')) do
-      local n = v.kW*v.kH*v.nInputPlane
-      v.weight:normal(0,math.sqrt(2/n))
-      if v.bias then v.bias:zero() end
-   end
-end
-
--- extract k items from each class 
+-- get k items from each class 
 function util.extractK(input, target, k, nClasses)
    if k*nClasses == target:size(1) then
       return input, target
@@ -225,25 +122,7 @@ function util.extractK(input, target, k, nClasses)
    return inputNew, targetNew
 end
 
-function util.extracKSpecial(input, target, k, nClasses)
-   local inputNew = torch.Tensor(k*nClasses, input:size(2), input:size(3), input:size(4)):typeAs(input)
-   local targetNew = torch.Tensor(k*nClasses):typeAs(target)  
- 
-   local found = {}
-   local idx = 1
-   for i=1,target:size(1) do
-      if not found[target[i]] then
-         inputNew[idx] = input[i]
-         targetNew[idx] = target[i]
-
-         idx = idx + 1
-         found[target[i]] = true 
-      end
-   end
-
-   return inputNew, targetNew:contiguous()
-end
-
+-- get random subset of items 
 function util.getRandomSubset(input, target, num)
    local shuffle = torch.randperm(input:size(1))
    input = input:index(1, shuffle:long())
@@ -255,42 +134,6 @@ function util.getRandomSubset(input, target, num)
       return input[{{1,num},{}}], target[{{1,num}}]
    end
 end
-
-function util.shuffleSplit(trainInput, trainTarget, testInput, testTarget)
-   local trainNum = trainInput:size(1)
-
-   -- concatenate
-   local concatInput = torch.clone(torch.cat(trainInput, testInput, 1))
-   local concatTarget = torch.clone(torch.cat(trainTarget, testTarget, 1))
-
-   -- shuffle
-   local shuffle = torch.randperm(concatInput:size(1))
-   concatInput = concatInput:index(1, shuffle:long())
-   concatTarget = concatTarget:index(1, shuffle:long())
-
-   local size = concatInput:size(1)
-   return concatInput[{{1,trainNum},{},{},{}}], concatTarget[{{1,trainNum}}], 
-          concatInput[{{trainNum+1,size},{},{},{}}], concatTarget[{{trainNum+1, size}}] 
-end
-
--- extract k items from each class 
-function util.convert(target, map)
-   local map = map or {}
-
-   local cnt = 1
-   local newTarget = torch.Tensor(target:size()):typeAs(target)
-   for i=1,target:size(1) do
-      if map[target[i]] == nil then
-         map[target[i]] = cnt 
-         cnt = cnt + 1
-      end 
-
-      newTarget[i] = map[target[i]]
-   end 
-
-   return newTarget, map 
-end
-
 
 function util.getBatch(input, target, idx, batchSize) 
    local x
